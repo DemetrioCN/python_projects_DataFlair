5a2bb4ebeae82bb274db16594f9d276b02351535
Demetrio <dem9411@gmail.com>
2020-07-01 19:26:27 -0500

    Python Story Generator added

---
+++ basic_desktop_notifier.py
@@ -0,0 +1,11 @@
+
+import notify2
+
+
+print('\n')
+print(10*"-","Desktop Notifier App" ,10*"-")
+print('\n')
+
+#notify2.init('Initialize project')
+
+
---
+++ basic_story_generator.py
@@ -0,0 +1,13 @@
+
+print('\n')
+print(10*'-','Python Story Generator' ,10*'-')
+print('\n')
+
+
+name = input('Introduce yoru name: ')
+action = input('Introduce what would you like to do tomorrow: ')
+hour = input('Introduce the start hour: ')
+
+
+message = f'Hi {name}, you should do {action} tomorrow at {hour} o\'clock'
+print(message)
